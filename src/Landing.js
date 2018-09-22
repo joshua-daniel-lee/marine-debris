b552069a9b5d3de67525331ef695834b761936a1
Joshua Lee <jdl93@nau.edu>
2018-09-22 11:15:09 -1000

    org of the files

---
+++ src/Landing.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Landing.css";
+
+class Landing extends Component {
+  render() {
+    return <div>test</div>;
+  }
+}
+
+export default Landing;
